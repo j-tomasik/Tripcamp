f068b14c46becd4d69d64ca4d0afccd05b2c5edd
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-01-21 15:53:55 -0800

    now renders div from app.jsx file to local host

--- frontend/trip_camp.jsx
+++ frontend/trip_camp.jsx
@@ -3,22 +3,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 //Components
 import Root from './components/root';
-import configureStore from './store/store';
+// import configureStore from './store/store';
 
 document.addEventListener('DOMContentLoaded', () => {
-    let store;
-    if (window.currentUser) {
-        const preloadedState = {
-            session: { id: window.currentUser.id },
-            entities: {
-                users: { [window.currentUser.id]: window.currentUser }
-            }
-        };
-        store = configureStore(preloadedState);
-        delete window.currentUser;
-    } else {
-        store = configureStore();
-    }
+    // let store;
+    // if (window.currentUser) {
+    //     const preloadedState = {
+    //         session: { id: window.currentUser.id },
+    //         entities: {
+    //             users: { [window.currentUser.id]: window.currentUser }
+    //         }
+    //     };
+    //     store = configureStore(preloadedState);
+    //     delete window.currentUser;
+    // } else {
+    //     store = configureStore();
+    // }
     const root = document.getElementById('root');
-    ReactDOM.render(<Root store={store} />, root);
+    ReactDOM.render(<Root />, root);
 });
