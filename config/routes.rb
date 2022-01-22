be595e37349dc3dc5f0b2b445f1623aff3ba8bf2
Jack Tomasik <jackedwardtomasik@gmail.com>
2022-01-22 13:06:45 -0800

    creating session actions and session reducer

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   namespace :api, defaults: {format: :json} do 
-    resources :users, only: [:create]
+    resources :user, only: [:create]
     resource :session, only: [:create, :destroy]
   end
 
